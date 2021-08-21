c2278e10152f2725d6b5796dd678982aaa9aa084
ZoxaCraft <89035218+ZoxaCraft@users.noreply.github.com>
2021-08-21 13:36:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ const API = require("./src/structure/API");
 let clash = new API();
 
 // Pour recherchez des informations à propos d'un joueur
-console.log(clash.showPlayerInfo("#RL9RJLL"));
+console.log(clash.showPlayerInfo("Ici le hashtag du joueur auquel vous recherchez des informations à propos de lui"));
